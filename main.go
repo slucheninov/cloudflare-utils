1de227f4b8b0777405e8628f63d7e5ef0e8a13fa
Viacheslav Lucheninov <slava.lucheninov@socialtechnologies.io>
2021-06-29 17:43:16 +0300

    update comments

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/cloudflare/cloudflare-go"
 )
 
-// Internet
+// bytes human readable (stackoverflow)
 func ByteCountIEC(b int64) string {
 	const unit = 1024
 	if b < unit {
@@ -26,22 +26,21 @@ func ByteCountIEC(b int64) string {
 }
 
 func main() {
-	//
+	// Init vars
 	var idzone = make(map[string]string)
+	// date UTC now
 	t := time.Now().UTC()
 	year, month, _ := t.Date()
 	firstDayOfThisMonth := time.Date(year, month, 1, 0, 0, 0, 0, t.Location())
+	// Init all sum
 	var summa int64
-
 	// Construct a new API object and get env api key
 	api, err := cloudflare.NewWithAPIToken(os.Getenv("CF_API_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// Most API calls require a Context
 	ctx := context.Background()
-
 	// Fetch user details on the account
 	u, err := api.UserDetails(ctx)
 	if err != nil {
